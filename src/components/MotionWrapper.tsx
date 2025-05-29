71672e5b511a57819de798ef3c36402edcd9f520
Brad Hanks <hanks.brad@gmail.com>
2025-05-29 11:22:35 -0600

    seperate motion client  concern

---
+++ src/components/MotionWrapper.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { motion, type motionValue } from 'framer-motion'
+
+export const MotionDiv = motion.div
+export const MotionSpan = motion.span
+export const MotionLi = motion.li
+export const MotionDt = motion.dt
+export const MotionDd = motion.dd
+export const MotionH1 = motion.h1
+export const MotionH2 = motion.h2
+export const MotionP = motion.p
+export const MotionPath = motion.path
+export const MotionSvg = motion.svg
