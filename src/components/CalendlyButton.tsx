85aedfd6e9c68cdeeaae3d9bdc6e866816e731c4
Brad Hanks <hanks.brad@gmail.com>
2025-05-29 23:30:39 -0600

    prettify

--- src/components/CalendlyButton.tsx
+++ src/components/CalendlyButton.tsx
@@ -30,7 +30,7 @@ export default function CalendlyButton({
             'active:text-emerald-500',
             'dark:bg-inherit dark:text-white',
             invert &&
-            'hover:text-emerald-500active:text-emerald-400 text-white',
+              'hover:text-emerald-500active:text-emerald-400 text-white',
           )}
           pageSettings={{
             backgroundColor: 'ffffff',
