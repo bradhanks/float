953f702c0ab9dcc72d24baf98a9f1794ae6b0d63
Brad Hanks <hanks.brad@gmail.com>
2025-06-07 15:48:23 -0600

    Add sitemap configuration and global styles; refactor  PostHog client and flags

--- src/lib/posthog/flags.ts
+++
@@ -1,9 +0,0 @@
-import PostHogClient from '@/lib/posthog/client'
-
-export async function getData() {
-  const posthog = PostHogClient()
-  const flags = await posthog.getAllFlags(
-    'ian@posthog.com', // replace with a user's distinct ID
-  )
-  return flags
-}
